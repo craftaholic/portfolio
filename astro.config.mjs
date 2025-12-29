882ab5f3bc17a48917fa76bbe5bde4b658bbe28c
Tommy Tran Duc Thang <tommytrandt.work@gmail.com>
2025-12-29 19:12:35 +0700

    feat(seo): improve seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 // @ts-check
+import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 import path from 'path';
 import { fileURLToPath } from 'url';
@@ -7,6 +8,8 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://craftaholic.sh',
+  integrations: [sitemap()],
   // Static site generation (default)
   // Deploy to Cloudflare Pages: bun run build && npx wrangler pages deploy dist
   prefetch: {
